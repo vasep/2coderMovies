d9629d7c6151a780614d4d3d4bc3aeb01051512e
Vasko Panov <mrvasep@gmail.com>
2022-03-10 22:26:07 +0100

    Project Init

---
+++ 2coderMovies/2coderMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2coderMovies
+//
+//  Created by Vasil Panov on 10.3.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
